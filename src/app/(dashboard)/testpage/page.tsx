5d7b0a8b9e46f1c6f34fbe93f1c3a09201bd9ca3
TeraSpheres <ahsan.ahmad1@gmail.com>
2026-07-23 18:12:39 -0600

    Clean up test pages

--- src/app/(dashboard)/testpage/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Test Page Works!</h1>
-      <p>If you see this, routing and builds are working correctly.</p>
-    </div>
-  );
-}

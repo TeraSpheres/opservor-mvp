8b0cc175847fefb668b1b7fffe2aa6a3756b1283
TeraSpheres <ahsan.ahmad1@gmail.com>
2026-07-23 18:04:40 -0600

    Add test page to debug routing

---
+++ src/app/(dashboard)/testpage/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Test Page Works!</h1>
+      <p>If you see this, routing and builds are working correctly.</p>
+    </div>
+  );
+}
